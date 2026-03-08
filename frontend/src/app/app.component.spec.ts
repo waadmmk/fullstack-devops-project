b184b21f3cbd983165dc28d17651a0f35a0964a9
waadmmk <waadalsubaie.111@gmail.com>
2026-03-08 15:07:13 +0300

    Update app.component.spec.ts

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,18 +1,17 @@
 import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule } from '@angular/forms';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule,
-        FormsModule,
-        RouterTestingModule
+      declarations: [
+        AppComponent
       ],
-      declarations: [AppComponent]
+      imports: [HttpClientTestingModule, FormsModule]
+
     }).compileComponents();
   });
 
@@ -32,6 +31,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.textContent).toContain('frontend');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
   });
 });
